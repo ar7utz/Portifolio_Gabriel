5f2234f495c78b9b865ed50dd49e7e28420943d8
ar7utz <pereira.artur@hotmail.com>
2024-08-14 21:18:58 -0300

    correção das propriedades de configuração do twind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+    "./**/*.{html,js}", //Inclui ficheiros HTML e JS do projeto
+    "!./node_modules",  //pra excluir os diretórios do node_modules
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
